23800175558314adba78eaf7a24822dced651c52
snehasharma <sneha.sharma@1click.tech>
2024-06-26 16:20:30 +0530

    firstcommit

--- components/Models/Slippers.js
+++ components/Models/Slippers.js
@@ -1,45 +1,10 @@
-import React, { useRef } from "react";
+import React, { forwardRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
-export function Slippers(props) {
-  const { nodes, materials } = useGLTF("/slippers.glb");
+export const Slippers = forwardRef((props, ref) => {
+  const { scene } = useGLTF("/gas.glb");
+  
+  return <primitive ref={ref} object={scene} {...props} />;
+});
 
-  return (
-    <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.soleUpper.geometry}
-        material={materials.soleUpper}
-        scale={0.799}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.strap1.geometry}
-        material={materials.strap}
-        position={[-0.546, 0.751, 0.017]}
-        rotation={[1.364, -0.4, -1.635]}
-        scale={0.629}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.strap2.geometry}
-        material={materials.strap}
-        position={[0.922, 0.394, 0.017]}
-        rotation={[1.364, -0.4, -1.635]}
-        scale={[0.726, 0.59, 0.387]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.soleBottom.geometry}
-        material={materials.soleBottom}
-        scale={0.799}
-      />
-    </group>
-  );
-}
-
-useGLTF.preload("/slippers.glb");
+useGLTF.preload("/gas.glb");
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,51 @@
+import React, { useRef, useEffect } from "react";
 import {
   ContactShadows,
   Environment,
   OrbitControls,
   PerspectiveCamera,
 } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { Slippers } from "../components/Models/Slippers";
 
-const index = () => {
+gsap.registerPlugin(ScrollTrigger);
+
+const SlippersAnimated = () => {
+  const slippersRef = useRef();
+
+  useEffect(() => {
+    if (slippersRef.current) {
+      const element = slippersRef.current;
+
+      ScrollTrigger.create({
+        trigger: "#scroll-container", // Trigger element
+        start: "top top", // Start when the top of the trigger element hits the top of the viewport
+        end: "bottom bottom", // End when the bottom of the trigger element hits the bottom of the viewport
+        scrub: true,
+        markers: true,
+        onUpdate: (self) => {
+          const progress = self.progress;
+          gsap.to(element.rotation, {
+            x: progress * 2 * Math.PI,
+            y: progress * 2 * Math.PI,
+          });
+          gsap.to(element.position, {
+            x: progress * 3,
+            y: progress * 2,
+          });
+        },
+      });
+    }
+  }, []);
+
+  return <Slippers ref={slippersRef} position={[0, 0.1, 0]} />;
+};
+
+const Index = () => {
   return (
-    <div className="h-[100vh]">
+    <div id="scroll-container" className="h-[200vh]"> {/* Increase height to enable scrolling */}
       <Canvas
         style={{
           width: "100%",
@@ -19,13 +54,13 @@ const index = () => {
       >
         <color attach="background" args={["#eee"]} />
         <Environment preset="studio" />
-        <PerspectiveCamera makeDefault position={[2, 3.9, 4.1]} />
-        <OrbitControls />
-        <Slippers position={[0, 0.1, 0]} />
+        <PerspectiveCamera makeDefault position={[0, 0, 3.1]} />
+        {/* <OrbitControls /> */}
+        <SlippersAnimated />
         <ContactShadows />
       </Canvas>
     </div>
   );
 };
 
-export default index;
+export default Index;
